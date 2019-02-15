127ebde0d7e9b8021a29aa6844750fb9535c8e38
Kasper Kimose Rasmussen <kkr@2bm.dk>
2019-02-15 09:45:41 +0100

    Adds evaluator, for evaluating models

---
+++ src/evaluator.py
@@ -0,0 +1,7 @@
+from base_model import BaseModel
+
+class Evaluator:
+    
+    def evaluate(self, model: BaseModel, x_test, y_test):
+        score = model.evaluate(x_test, y_test, verbose=0)
+        print("val_loss = {:.3f} and val_acc = {:.3f}".format(score[0], score[1]))
