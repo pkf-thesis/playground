7a7ce370eec8855309837e186ead8d12277d35c3
Peter <peter.html@gmail.com>
2019-02-15 10:53:40 +0100

    select amount of tids pr tags

---
+++ src/sqllite_truncater.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import sqlite3
+
+# Example usage
+#print (fetchTagFromSong("TRCCCYE12903CFF0E9"))
+#songTags = fetchTagsFromSongs(["TRCCCYE12903CFF0E9", "TRAAABD128F429CF47"])
+#print (songTags.get("TRAAABD128F429CF47"))
+
+def database():
+    dbfile = "../db/lastfm_tags.db"
+
+    if not os.path.isfile(dbfile):
+        print ('ERROR: db file %s does not exist' % dbfile)
+        sys.exit(0)
+    return dbfile
+
+def deleteEverythingButTop50Tags():
+    dbfile = database()
+    conn = sqlite3.connect(dbfile)
+
+    sql = "SELECT tag, COUNT(tid) AS countTid FROM tid_tag GROUP BY tag ORDER BY countTid DESC LIMIT 10"
+    res = conn.execute(sql)
+    data = res.fetchall()
+
+    print(data)
+
+    conn.close()
+
+# Main
+database()
+deleteEverythingButTop50Tags()
+
+# DELETE 
+# FROM dbo.ErrorLog
+# WHERE  
+# ErrorLogId NOT IN ( SELECT TOP ( 2 )
+#                             ErrorLogId
+#                     FROM    dbo.ErrorLog
+#                     ORDER BY ErrorDate )
