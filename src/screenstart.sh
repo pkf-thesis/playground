c4598115862c99e3269c0e4cb9d83a721e753a03
Peter <peter.html@gmail.com>
2019-03-11 15:30:46 +0100

    remove cd in screenstar t

--- src/screenstart.sh
+++ src/screenstart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #perl -pi.bak -e 's/\r$//' screenstart.sh
 #if ! ps -ef | grep -v grep | grep $1 ; then
-    cd /usr/local/share/FKP/src
+#   cd /usr/local/share/FKP/src
     echo Adding $1 to screens...
     screen -dmS $1 python main.py -d=gtzan -logging=../logs
     screen -list
