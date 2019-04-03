1a8c1e36862cd791a6bb25257102035d7324226f
Frederik Dam Christensen <fdc@shape.dk>
2019-04-03 19:41:21 +0200

    calculate mean roc_auc score

--- src/evaluator.py
+++ src/evaluator.py
@@ -2,7 +2,7 @@ from typing import List
 
 from models.base_model import BaseModel
 from data_generator import DataGenerator
-
+from sklearn.metrics import roc_auc_score
 import numpy as np
 
 
@@ -27,3 +27,16 @@ class Evaluator:
         # song = song.reshape(1, 3 * 3**9, 1)
         # prediction = model.predict(song)
         # print(prediction)
+
+    # Example
+    # predictions   = array([[0.54, 0.98, 0.43], [0.32, 0.18, 0.78], [0.78, 0.76, 0.86]])
+    # truths        = array([[1, 1, 0], [0, 0, 1], [1, 1, 0]])
+    # mean_roc_auc  = 0.66
+    def mean_roc_auc(predictions, truths):
+        n_predictions = len(predictions)
+        auc = np.zeros(n_predictions)
+        for index in range(n_predictions):
+            prediction = predictions[index]
+            truth = truths[index]
+            auc[index] = roc_auc_score(truth, prediction)
+        return np.mean(auc)
