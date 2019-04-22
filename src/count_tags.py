9d171f3fd83309ce8bc574e1345897a61169ff8a
KasperKimose <kasper-r@hotmail.com>
2019-04-22 10:14:13 +0200

    Adds count_tags

---
+++ src/count_tags.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+train = np.load("../data/mtat/y_train_pub.npy")
+valid = np.load("../data/mtat/y_valid_pub.npy")
+test = np.load("../data/mtat/y_test_pub.npy")
+
+for i in range(0, 50):
+    print(np.sum(train[...,i]))
+
+print("\n")
+
+for i in range(0, 50):
+    print(np.sum(valid[...,i]))
+
+print("\n")
+
+for i in range(0, 50):
+    print(np.sum(test[:, i]))
+
+
