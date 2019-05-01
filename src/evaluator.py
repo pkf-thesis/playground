bd16abe06547720c5d7b10a8c80771af717e1fa6
Frederik Dam Christensen <fdc@shape.dk>
2019-05-01 10:18:59 +0200

    norm correct axis

--- src/evaluator.py
+++ src/evaluator.py
@@ -50,7 +50,8 @@ def confusion_matrix(predictions, truths):
         truth_matrix = np.repeat(np.array([norm_truth]), n_labels, axis=0)
         cm_temp = pred_matrix * truth_matrix
         cm = np.add(cm_temp, cm)
-        norm_cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        #norm_cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        norm_cm = cm/cm.sum(axis=0,keepdims=1)
     return norm_cm
 
 # Example
