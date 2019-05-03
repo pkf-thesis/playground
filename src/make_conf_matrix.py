fb9239ecaf416bca5040004c9220c871eeff6e3c
KasperKimose <kasper-r@hotmail.com>
2019-05-03 10:43:33 +0200

    Adds script to generate conf matrix. Adds arg for cross. refactors to run deep_res_net

---
+++ src/make_conf_matrix.py
@@ -0,0 +1,9 @@
+import sys
+import numpy as np
+from evaluator import plot_confusion_matrix2
+
+predictions = np.load(sys.argv[1])
+truths = np.load(sys.argv[2])
+labels = [label.rstrip() for label in open("../data/mtat/tags.txt")]
+
+confusion_matrix = plot_confusion_matrix2(predictions, truths, labels)
